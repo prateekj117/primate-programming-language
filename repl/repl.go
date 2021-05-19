644861d7df101213b7e5560660f72a7f20274212
Prateek Jain <prateekj1171998@gmail.com>
2021-05-19 17:21:04 +0530

    Extend REPL for properly logging errors

--- repl/repl.go
+++ repl/repl.go
@@ -9,11 +9,28 @@ import (
 	"io"
 
 	"Primate/lexer"
-	"Primate/token"
+	"Primate/parser"
 )
 
 // PROMPT is the REPL prompt displayed for each input
 const PROMPT = ">> "
+
+// ComputerFace is the REPL's face of shock and horror when you encounter a
+// parser error :D
+const ComputerFace = `
+   _______________  
+  |  ___________  | 
+  | |           | | 
+  | |   0   0   | | 
+  | |     -     | | 
+  | |   \___/   | | 
+  | |___     ___| | 
+  |_____|\_/|_____| 
+    _|__|/ \|_|_
+   / ********** \   
+ /  ************  \ 
+--------------------
+`
 
 // Start starts the REPL in a continious loop
 func Start(in io.Reader, out io.Writer) {
@@ -28,9 +45,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, ComputerFace)
+	io.WriteString(out, "Woops! We ran into some primate business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
